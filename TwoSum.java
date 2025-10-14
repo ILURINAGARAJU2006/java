5b3ab7e154109a9278b0c2ebaf19e2bbb90eddba
ILURINAGARAJU2006 <ilurinagaraju1010@gmail.com>
2025-10-14 21:24:05 +0530

    TwoSum.java
    leetcode program 1

---
+++ TwoSum.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target)
+                    return new int[] { i, j };
+            }
+        }
+        return new int[] {};
+    }
+}
+
+class Example {
+    public static void main(String args[]) {
+        Solution s = new Solution();
+        int arr[] = { 2, 7, 11, 15 };
+        s.twoSum(arr, 9);
+    }
+}
